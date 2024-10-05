2c72bccb3149b8b08277a139d38a98a9e08b5b4d
Sondre Sætervadet <sondsae@stud.ntnu.no>
2024-10-05 16:14:51 +0200

    got posts to work

--- scripts.js
+++ scripts.js
@@ -1,10 +1,47 @@
-function fetchHomeData() {
+let posts = null;
+
+let k = 0;
+
+async function fetchHomeData() {
     //API Endpoint: https://jsonplaceholder.typicode.com/posts
-    const response = fetch("https://jsonplaceholder.typicode.com/posts")
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
         if (!response.ok) {
-            throw new Error("Error with the status: " + response.status);
+            throw new Error("Error with the status: ");
         }
-        console.log(response)
-    
-    
+        posts = await response.json();
 }
+
+function makepost(title, body) {
+    const container = document.createElement("div");
+        container.classList.add("post");
+
+        const header = document.createElement("h2");
+        header.textContent = title;
+        container.appendChild(header);
+
+        const paragraph = document.createElement("p");
+        paragraph.textContent = body;
+        container.appendChild(paragraph);
+
+        document.getElementById("main-container").appendChild(container);
+}
+
+const bottomIsVisible = () => // for recognizing scrolling down
+    document.documentElement.clientHeight + window.scrollY >=
+    document.documentElement.scrollHeight - 100;
+
+async function main() {
+    await fetchHomeData()
+    for (let i = 0; i < 9; i++) {
+        makepost(posts[k++].title,posts[k].body);
+    }
+    addEventListener("scroll", () => {
+        if (bottomIsVisible()) {
+            for (let i = 0; i < 3; i++) {
+                makepost(posts[k++].title,posts[k].body);
+            }
+        }
+    });
+}
+
+main()
